813d149db8d81ab39e17dbeb481b64272c80fcea
demingry <demingry@sina.com>
2022-09-15 23:28:37 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -9,11 +9,3 @@
 #sudo apt update
 #sudo apt install -y neofetch
 
-
-curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-
-sudo apt update
-
-sudo apt install cloudflare-warp
